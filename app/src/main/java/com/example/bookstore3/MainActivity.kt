7707d09cbf38f4d7de1a4f8498ea32a4ccd21fb0
chunwenng <chunwen.ng@softspace.com.my>
2024-08-28 09:45:49 +0800

    28-Aug-2024 09.45am

--- app/src/main/java/com/example/bookstore3/MainActivity.kt
+++ app/src/main/java/com/example/bookstore3/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.bookstore3
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.bookstore3.ui.theme.Bookstore3Theme
 import com.example.bookstore3.navigation.Navigation
 import io.github.jan.supabase.createSupabaseClient
@@ -15,7 +20,6 @@ import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.storage.Storage
 import kotlinx.coroutines.launch
 
-
 val supabase = createSupabaseClient(
     supabaseUrl = "https://gilbcsillejbghvnrwbw.supabase.co",
     supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpbGJjc2lsbGVqYmdodm5yd2J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQzNzU5NTIsImV4cCI6MjAzOTk1MTk1Mn0.1EHEIFPZjv2y_G8jm6nDx_iq5gsclBYALHYw5S-Nx7c"
@@ -24,13 +28,21 @@ val supabase = createSupabaseClient(
     install(Storage)
 }
 
-
-
 class MainActivity : ComponentActivity() {
+
+    private val permissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Check and request permissions
+        checkAndRequestPermissions()
 
         signInAnonymously()
 
@@ -46,19 +58,21 @@ class MainActivity : ComponentActivity() {
         lifecycleScope.launch {
             try {
                 val user = supabase.auth.signInAnonymously()
-                Log.d("Auth", "Signed in as anonymous user: ${user}")
+                Log.d("Auth", "Signed in as anonymous user: $user")
             } catch (e: Exception) {
                 Log.e("Auth", "Sign in error: ${e.message}")
             }
         }
     }
-//    private fun signInAnonymously() {
-//        lifecycleScope.launch {
-//            try {
-//                supabase.auth.signInAnonymously(captchaToken = "token")
-//            } catch (e: Exception) {
-//                Log.e("Auth", "Sign in error: ${e.message}")
-//            }
-//        }
-//    }
+
+    // Function to check and request permissions
+    private fun checkAndRequestPermissions() {
+        val notGrantedPermissions = permissions.filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+
+        if (notGrantedPermissions.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, notGrantedPermissions.toTypedArray(), 0)
+        }
+    }
 }
